4fab8eb1f36ed16f1fbc7d748627630e55a5d3d3
Ahmed Khalil <ahmedkhalil91518@gmail.com>
2022-03-10 22:29:42 +0200

    create the basic structure of the front page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,16 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Navigation from "./components/Navigation";
 function App() {
-  return <Navigation />;
+  return (
+    <>
+      <Navigation />
+      <div className="shapes-container">
+        <div class="shape"></div>
+        <div class="shape2"></div>
+        <div className="text-container"></div>
+      </div>
+    </>
+  );
 }
 
 export default App;
